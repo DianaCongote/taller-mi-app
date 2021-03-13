bb24d41df3ae7c37edcdd4dabcbe698dc5307332
Diana <dianacongote00@gmail.com>
2021-03-13 11:05:37 -0500

    taller individual diana

--- src/app/nosotros/nosotros.page.ts
+++ src/app/nosotros/nosotros.page.ts
@@ -12,27 +12,37 @@ export class NosotrosPage implements OnInit {
   numero2 = null;
   resultado = null;
   mensaje = null;
+  signo = null;
+  igual = null;
   constructor() { }
 
   haceroperacion(){
     if (this.operacion=='+'){
       this.resultado = parseInt(this.numero1) + parseInt(this.numero2)
       this.mensaje  = 'la suma de '
+      this.signo = '+'
+      this.igual = '='
       console.log(this.resultado)
     }
     if (this.operacion=='-'){
       this.resultado = this.numero1 - this.numero2
       this.mensaje  = 'la resta de '
+      this.signo = '-'
+      this.igual = '='
       console.log(this.resultado)
     }
     if (this.operacion=='*'){
       this.resultado = this.numero1 * this.numero2
       this.mensaje  = 'la multiplicacion de '
+      this.signo = '*'
+      this.igual = '='
       console.log(this.resultado)
     }
     if (this.operacion=='/'){
       this.resultado = this.numero1 / this.numero2
       this.mensaje  = 'la division de '
+      this.signo = '/'
+      this.igual = '='
       console.log(this.resultado)
     }
       
