80456b0d77f2d3020f61e9676bc51f9e594ed6ed
Diana <dianacongote00@gmail.com>
2021-03-12 19:29:22 -0500

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,81 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  nombre = null;
+  apellido = null;
+  celular = null;
+  email = null;
+  id = null;
+
+  usuarios = [];
+
+  constructor() { }
+
+  guardarDatos() {
+    console.log(this.id)
+    if(this.id != null){
+      this.usuarios[this.id]["nombre"] = this.nombre
+      this.usuarios[this.id]["apellido"] = this.apellido
+      this.usuarios[this.id]["celular"] = this.celular
+      this.usuarios[this.id]["email"] = this.email
+
+      this.nombre = "";
+      this.apellido = "";
+      this.celular = "";
+      this.email = "";
+      this.id = null
+
+    }else{
+    // Agregando datos a la base de datos en memoria
+    this.usuarios.push({
+      nombre: this.nombre,
+      apellido: this.apellido,
+      celular: this.celular,
+      email: this.email
+
+    })
+    // Limpiando datos
+    this.nombre = "";
+    this.apellido = "";
+    this.celular = "";
+    this.email = "";
+    this.id = null
+  }}
+  
+
+
+  editarDatos(item) {
+    this.nombre = item["nombre"];
+    this.apellido = item["apellido"];
+    this.celular = item["celular"];
+    this.email = item["email"];
+    this.id = this.usuarios.indexOf(item)
+  
+    this.usuarios[this.id]({
+      nombre: this.nombre,
+      apellido: this.apellido,
+      celular: this.celular,
+      email: this.email
+    })
+    // Limpiando datos
+    this.nombre = "";
+    this.apellido = "";
+    this.celular = "";
+    this.email = "";
+    this.id = null
+    
+  }
+
+
+  borrarDatos(item) {
+    // Agregando datos a la base de datos en memoria
+    let id = this.usuarios.indexOf(item)
+    this.usuarios.splice(id,1)
+  }
+
+}
