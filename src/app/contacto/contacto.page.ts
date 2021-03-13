10a4820e0f9992af0e5d7292dc690a97854bf1f3
Diana <dianacongote00@gmail.com>
2021-03-13 10:44:10 -0500

    taller en clase

--- src/app/contacto/contacto.page.ts
+++ src/app/contacto/contacto.page.ts
@@ -7,8 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactoPage implements OnInit {
 
+  nombre = null;
   constructor() { }
-
+  saludar(){
+    if (this.nombre == null){
+      console.log('no hay nombre')
+      alert("ingrese su nombre para el saludo")
+    }else{
+      console.log(this.nombre)
+      alert("Hola "+ this.nombre)
+    }
+  }
+  
   ngOnInit() {
   }
 
--- src/app/nosotros/nosotros.page.ts
+++ src/app/nosotros/nosotros.page.ts
@@ -7,7 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NosotrosPage implements OnInit {
 
+  numero1 = null;
+  operacion = null;
+  numero2 = null;
+  resultado = null;
+  mensaje = null;
   constructor() { }
+
+  haceroperacion(){
+    if (this.operacion=='+'){
+      this.resultado = parseInt(this.numero1) + parseInt(this.numero2)
+      this.mensaje  = 'la suma de '
+      console.log(this.resultado)
+    }
+    if (this.operacion=='-'){
+      this.resultado = this.numero1 - this.numero2
+      this.mensaje  = 'la resta de '
+      console.log(this.resultado)
+    }
+    if (this.operacion=='*'){
+      this.resultado = this.numero1 * this.numero2
+      this.mensaje  = 'la multiplicacion de '
+      console.log(this.resultado)
+    }
+    if (this.operacion=='/'){
+      this.resultado = this.numero1 / this.numero2
+      this.mensaje  = 'la division de '
+      console.log(this.resultado)
+    }
+      
+  }
 
   ngOnInit() {
   }
